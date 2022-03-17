a38640340cbfd180b822beeae9753635c8895fdf
Meena <Meenakshisundaram.t@gmail.com>
2022-03-17 14:18:06 -0400

    moving the responsible ML to class model

--- framework/responsibleML/library/aigovernance/responsibleML.py
+++ framework/responsibleML/library/aigovernance/responsibleML.py
@@ -1,46 +1,100 @@
-import math
-
-explainable = False
-emissions = 0.0
-bias = 0.0
-diff_privacy = False
-
-def set_explainability(isexplainaable: bool):
-    explainable = isexplainaable
+class rML:
     
-def set_emissions(emissions: float):
-    emissions = emissions
+    __modeltype__ = ""
+    __emissions__ = 0.0
+    __bias__ = 0.0
+    __explained__ = False
+    __epsilon__ = 0.0
 
-def set_bias(bias: float):
-    bias = bias
+    def __init__(self, modeltype:str,
+                 explained:bool,
+                 emissions:float,
+                 bias:float,
+                 epsilon:float):
 
-def set_diff_privacy(isdiffprivacy: bool):
-    diff_privacy = isdiffprivacy
-
-def rai_index():
-    index = 0.0
+        self.__modeltype__ = modeltype
+        self.__emissions__ = emissions
+        self.__bias__ = bias
+        self.__explained__ = explained
+        self.__epsilon__ = epsilon
     
-    #calcuate explainability index
-    exp_index = 0.0
-    if explainable == True:
-        exp_index = 3.0
-    else:
-        exp_index = 1.0
+    def explained(self, isexplained: bool):
+        self.__explained__ = isexplained
+
+    def emissions(self, carbon_emissions: float):
+        self.__emissions__ = carbon_emissions
+
+    def bias(self, label_bias: float):
+        self.__bias__ = label_bias
+
+    def epsilon(self, privacy_epsilon: bool):
+        self.__epsilon__ = privacy_epsilon
+
+    def model(self, modeltype: str):
+        self.__modeltype__ = modeltype
         
-    #calcuate emissions index
-    if emissions > 0.50 and emissions < 1.0:
-        emissions_index = 2.0
-    elif emissions >= 1.0:
-        emissions_index = 1.0
-    else:
-        emissions_index = 3.0
-        
-    #calcuate Privacy index
-    priv_index = 0.0
-    if diff_privacy == True:
-        priv_index = 3.0
-    else:
-        priv_index = 1.0
+    def __calculate_emissions_index(self):
+
+        print(self.__emissions__)
+
+        if self.__emissions__ <= 500:
+            emissionIndex = 3
+        elif self.__emissions__ > 500 and self.__emissions__ <= 10000:
+            emissionIndex = 2
+        else:
+            emissionIndex = 1
+
+        return emissionIndex
+
+    def __calculate_privacy_index(self):
+        if self.__epsilon__ <= 1:
+            privacyIndex = 3
+        elif self.__epsilon__ > 1 and self.__epsilon__ <= 10:
+            privacyIndex = 2
+        else:
+            privacyIndex = 1
+
+        return privacyIndex
+
+    def __calculate_explainability_index(self):
+
+        expIndex = 1
+
+        if self.__modeltype__ == "deeplearning":
+            return expIndex
+
+        if self.__explained__ == True:
+            expIndex = 3
+        else:
+            expIndex = 2
+
+        return expIndex
+
+    def __calculate_bias_index(self):
+        if self.__bias__ <= 0.25 and self.__bias__ >= -0.25:
+            bindex = 3
+        elif self.__bias__ > 0.5 or self.__bias__ < -0.5:
+            bindex = 1
+        else:
+            bindex = 2
+
+        return bindex
     
-    index = (exp_index + emissions_index + priv_index) / 3.0
-    return index
+    def rai_index(self):
+    
+        index = 0.0
+        weights = 0.2
+
+        emission_index = self.__calculate_emissions_index()
+        privacy_index = self.__calculate_privacy_index()
+        bias_index = self.__calculate_bias_index()
+        explain_index = self.__calculate_explainability_index()
+
+        print(emission_index)
+        print(privacy_index)
+        print(bias_index)
+        print(explain_index)
+
+        index = weights * (emission_index + privacy_index + bias_index + explain_index)
+
+        return index
--- framework/responsibleML/library/setup.py
+++ framework/responsibleML/library/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='responsibleML',
     packages=setuptools.find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='Responsible ML',
     author='Meenakshisundaram.t@gmail.com',
     license='MIT',
